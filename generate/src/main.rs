6c485ee5535e198b8cc86d13206b95ef3f205358
cydparser <cydparser@gmail.com>
2025-03-05 22:16:33 -0800

    Extract ForeignItemFn branch into fn

--- generate/src/main.rs
+++ generate/src/main.rs
@@ -1,9 +1,9 @@
-use quote::{format_ident, quote};
+use quote::format_ident;
 use std::{
     fs,
     path::{Path, PathBuf},
 };
-use syn::{token::PathSep, Ident, Item, Token, UseName, UsePath, UseTree};
+use syn::{parse_quote, Ident, Item, Visibility};
 
 fn main() {
     let src_dir = PathBuf::from(String::from(env!("OUT_DIR")));
@@ -26,8 +26,10 @@ fn main() {
         let mod_dir = main_rs.with_extension("");
 
         let code = fs::read_to_string(path).unwrap();
-        let (main_file, tests_file) =
-            transform_tree(syn::parse_file(&code).unwrap());
+        let Transformed {
+            main_file,
+            tests_file,
+        } = transform_tree(syn::parse_file(&code).unwrap());
 
         fs::create_dir_all(&mod_dir)?;
         fs::write(&main_rs, prettyplease::unparse(&main_file).as_bytes())?;
@@ -61,119 +63,58 @@ where
     Ok(())
 }
 
-fn transform_tree(syn_file: syn::File) -> (syn::File, syn::File) {
-    let mut main_file = syn::File {
-        shebang: None,
-        attrs: vec![],
-        items: Vec::with_capacity(syn_file.items.len()),
+struct Transformed {
+    main_file: syn::File,
+    tests_file: syn::File,
+}
+
+fn transform_tree(syn_file: syn::File) -> Transformed {
+    let mut transformed = Transformed {
+        main_file: syn::File {
+            shebang: None,
+            attrs: vec![],
+            items: Vec::with_capacity(syn_file.items.len()),
+        },
+        tests_file: syn::File {
+            shebang: None,
+            attrs: vec![],
+            items: vec![
+                Item::Use(parse_quote! { use ghc_rts_sys as sys; }),
+                Item::Use(parse_quote! { use quickcheck::quickcheck; }),
+            ],
+        },
     };
     let mut items = syn_file.items.into_iter().peekable();
 
-    main_file
+    transformed
+        .main_file
         .items
         .push(Item::Use(parse_quote! { use ghc_rts_sys as sys; }));
+
     // Add original imports.
     while let Some(item) = items.peek() {
         match item {
-            Item::Use(_) => main_file.items.push(items.next().unwrap()),
+            Item::Use(_) => transformed.main_file.items.push(items.next().unwrap()),
             _ => break,
         }
     }
-    main_file.items.push(Item::Mod(parse_quote! {
+
+    transformed.main_file.items.push(Item::Mod(parse_quote! {
         #[cfg(test)]
         mod tests;
     }));
-
-    let mut tests_file = syn::File {
-        shebang: None,
-        attrs: vec![],
-        items: vec![
-            Item::Use(parse_quote! { use ghc_rts_sys as sys; }),
-            Item::Use(parse_quote! { use quickcheck::quickcheck; }),
-        ],
-    };
 
     for item in items {
         match item {
             Item::Const(mut item_const) => {
                 item_const.vis = Visibility::Inherited;
-                main_file.items.push(Item::Const(item_const));
+                transformed.main_file.items.push(Item::Const(item_const));
             }
             Item::ForeignMod(foreign_mod) => {
                 for fitem in foreign_mod.items.into_iter() {
                     match fitem {
-                        syn::ForeignItem::Fn(syn::ForeignItemFn {
-                            sig:
-                                syn::Signature {
-                                    ident,
-                                    generics,
-                                    inputs,
-                                    variadic,
-                                    output,
-                                    ..
-                                },
-                            ..
-                        }) => {
-                            assert!(generics.gt_token.is_none() && generics.where_clause.is_none());
-
-                            // TODO: There are variadic functions in rts::messages.
-                            if variadic.is_some() {
-                                eprintln!("Ignoring variadic function: {}", ident);
-                                continue;
-                            }
-                            let args: Vec<Ident> = inputs
-                                .iter()
-                                .map(|arg| match arg {
-                                    syn::FnArg::Typed(syn::PatType { pat, .. }) => {
-                                        if let syn::Pat::Ident(
-                                            ref pat_ident @ syn::PatIdent { .. },
-                                        ) = **pat
-                                        {
-                                            pat_ident.ident.clone()
-                                        } else {
-                                            panic!(
-                                                "Expected only syn::Pat::Ident variants: {:}",
-                                                &ident
-                                            );
-                                        }
-                                    }
-                                    syn::FnArg::Receiver(_) => {
-                                        panic!("Unexpected FnArg::Recever: {:}", &ident)
-                                    }
-                                })
-                                .collect();
-
-                            let func = parse_quote! {
-                                #[unsafe(no_mangle)]
-                                pub extern "C" fn #ident(#inputs) #output {
-                                    unsafe { sys::#ident(#(#args),*) }
-                                }
-                            };
-                            main_file.items.push(Item::Fn(func));
-                            if let syn::ReturnType::Type(_, _) = output {
-                                let fn_ident = format_ident!("equivalent_{}", ident);
-
-                                // TODO: Convert types using trait like To
-                                tests_file.items.push(Item::Fn(parse_quote! {
-                                    #[cfg(feature = "sys")]
-                                    #[quickcheck]
-                                    fn #fn_ident(#inputs) -> bool {
-                                        let expected = unsafe { sys::#ident(#(#args),*) };
-                                        super::#ident(#(#args),*) == expected
-                                    }
-                                }));
-                            } else {
-                                // TODO: Should this still be a property test?
-                                let fn_ident = format_ident!("test_{}", ident);
-
-                                tests_file.items.push(Item::Fn(parse_quote! {
-                                    #[test]
-                                    fn #fn_ident() {
-                                        super::#ident(#(#args),*);
-                                        todo!()
-                                    }
-                                }));
-                            }
+                        syn::ForeignItem::Fn(ffn) => {
+                            transform_ffn(ffn, &mut transformed);
                         }
                         syn::ForeignItem::Static(syn::ForeignItemStatic {
                             ident,
@@ -182,7 +123,7 @@ fn transform_tree(syn_file: syn::File) -> (syn::File, syn::File) {
                             ..
                         }) => {
                             // TODO: Determine witch statics must be exported.
-                            main_file.items.push(Item::Static(parse_quote! {
+                            transformed.main_file.items.push(Item::Static(parse_quote! {
                                 static #mutability #ident: #ty = ghc_rts_sys::#ident;
                             }));
                         }
@@ -203,5 +144,81 @@ fn transform_tree(syn_file: syn::File) -> (syn::File, syn::File) {
         }
     }
 
-    (main_file, tests_file)
+    transformed
+}
+
+fn transform_ffn(ffn: syn::ForeignItemFn, transformed: &mut Transformed) {
+    let syn::ForeignItemFn {
+        sig:
+            syn::Signature {
+                ident,
+                generics,
+                inputs,
+                variadic,
+                output,
+                ..
+            },
+        ..
+    } = ffn;
+
+    assert!(generics.gt_token.is_none() && generics.where_clause.is_none());
+
+    let Transformed {
+        main_file,
+        tests_file,
+    } = transformed;
+
+    // TODO: There are variadic functions in rts::messages.
+    if variadic.is_some() {
+        eprintln!("Ignoring variadic function: {}", ident);
+        return;
+    }
+    let args: Vec<Ident> = inputs
+        .iter()
+        .map(|arg| match arg {
+            syn::FnArg::Typed(syn::PatType { pat, .. }) => {
+                if let syn::Pat::Ident(ref pat_ident @ syn::PatIdent { .. }) = **pat {
+                    pat_ident.ident.clone()
+                } else {
+                    panic!("Expected only syn::Pat::Ident variants: {:}", &ident);
+                }
+            }
+            syn::FnArg::Receiver(_) => {
+                panic!("Unexpected FnArg::Recever: {:}", &ident)
+            }
+        })
+        .collect();
+
+    let func = parse_quote! {
+        #[unsafe(no_mangle)]
+        pub extern "C" fn #ident(#inputs) #output {
+            unsafe { sys::#ident(#(#args),*) }
+        }
+    };
+    main_file.items.push(Item::Fn(func));
+    if let syn::ReturnType::Type(_, _) = output {
+        let fn_ident = format_ident!("equivalent_{}", ident);
+
+        // TODO: Convert types using trait like From
+        // TODO: Convert pointers to pass-by-value
+        tests_file.items.push(Item::Fn(parse_quote! {
+            #[cfg(feature = "sys")]
+            #[quickcheck]
+            fn #fn_ident(#inputs) -> bool {
+                let expected = unsafe { sys::#ident(#(#args),*) };
+                super::#ident(#(#args),*) == expected
+            }
+        }));
+    } else {
+        // TODO: Should this still be a property test?
+        let fn_ident = format_ident!("test_{}", ident);
+
+        tests_file.items.push(Item::Fn(parse_quote! {
+            #[test]
+            fn #fn_ident() {
+                super::#ident(#(#args),*);
+                todo!()
+            }
+        }));
+    }
 }
