e039b83b5a089a295535ace198a13a0ab0757683
cydparser <cydparser@gmail.com>
2025-03-12 21:32:13 -0700

    Use more robust foreign function conversion

--- generate/src/main.rs
+++ generate/src/main.rs
@@ -1,9 +1,10 @@
+use proc_macro2::{Span, TokenStream};
 use quote::format_ident;
 use std::{
     fs,
     path::{Path, PathBuf},
 };
-use syn::{parse_quote, Ident, Item, Visibility};
+use syn::{parse_quote, punctuated::Punctuated, token, Ident, Item, Visibility};
 
 fn main() {
     let src_dir = PathBuf::from(String::from(env!("OUT_DIR")));
@@ -173,25 +174,63 @@ fn transform_ffn(ffn: syn::ForeignItemFn, transformed: &mut Transformed) {
         eprintln!("Ignoring variadic function: {}", ident);
         return;
     }
-    let args: Vec<Ident> = inputs
+
+    let (inputs_owned, args, args_into, args_from_owned, bindings): (
+        Punctuated<_, token::Comma>,
+        Vec<_>,
+        Vec<_>,
+        Vec<_>,
+        Vec<_>,
+    ) = inputs
         .iter()
         .map(|arg| match arg {
-            syn::FnArg::Typed(syn::PatType { pat, .. }) => {
-                if let syn::Pat::Ident(ref pat_ident @ syn::PatIdent { .. }) = **pat {
-                    pat_ident.ident.clone()
+            syn::FnArg::Typed(pat_type) => {
+                if let syn::Pat::Ident(pat_ident @ syn::PatIdent { .. }) = pat_type.pat.as_ref() {
+                    let param_ident = pat_ident.ident.clone();
+
+                    let (ty_owned, arg_into, arg_from_owned) = match pat_type.ty.as_ref() {
+                        ty @ syn::Type::Path(_) => (
+                            ty.clone(),
+                            expr_into(&param_ident),
+                            syn::Pat::Ident(new_pat_ident(&param_ident)),
+                        ),
+                        syn::Type::Ptr(type_ptr) => ptr_to_ty_expr_pat(&param_ident, type_ptr),
+                        ty => panic!("Unexpected type in {:?}: {:?}", arg, ty),
+                    };
+
+                    let binding: TokenStream =
+                        format!("let {} = Default::default();", &param_ident)
+                            .parse()
+                            .unwrap_or_else(|e| {
+                                panic!("Unable to parse let expression: {:?} {}", arg, e)
+                            });
+
+                    (
+                        syn::FnArg::Typed(syn::PatType {
+                            attrs: pat_type.attrs.clone(),
+                            pat: pat_type.pat.clone(),
+                            colon_token: pat_type.colon_token.clone(),
+                            ty: Box::new(ty_owned),
+                        }),
+                        param_ident,
+                        arg_into,
+                        arg_from_owned,
+                        binding,
+                    )
                 } else {
-                    panic!("Expected only syn::Pat::Ident variants: {:}", &ident);
+                    panic!("Expected only syn::Pat::Ident: {:#?}", arg);
                 }
             }
             syn::FnArg::Receiver(_) => {
-                panic!("Unexpected FnArg::Recever: {:}", &ident)
+                panic!("Unexpected FnArg::Recever: {:#?}", arg)
             }
         })
         .collect();
 
+    // Mark all functions as unsafe until the code can be audited.
     let func = parse_quote! {
         #[unsafe(no_mangle)]
-        pub extern "C" fn #ident(#inputs) #output {
+        pub unsafe extern "C" fn #ident(#inputs) #output {
             unsafe { sys::#ident(#(#args),*) }
         }
     };
@@ -199,26 +238,69 @@ fn transform_ffn(ffn: syn::ForeignItemFn, transformed: &mut Transformed) {
     if let syn::ReturnType::Type(_, _) = output {
         let fn_ident = format_ident!("equivalent_{}", ident);
 
-        // TODO: Convert types using trait like From
-        // TODO: Convert pointers to pass-by-value
         tests_file.items.push(Item::Fn(parse_quote! {
             #[cfg(feature = "sys")]
             #[quickcheck]
-            fn #fn_ident(#inputs) -> bool {
-                let expected = unsafe { sys::#ident(#(#args),*) };
-                super::#ident(#(#args),*) == expected
-            }
-        }));
-    } else {
-        // TODO: Should this still be a property test?
-        let fn_ident = format_ident!("test_{}", ident);
-
-        tests_file.items.push(Item::Fn(parse_quote! {
-            #[test]
-            fn #fn_ident() {
-                super::#ident(#(#args),*);
-                todo!()
+            fn #fn_ident(#inputs_owned) -> bool {
+                let expected = unsafe { sys::#ident(#(#args_into),*) };
+                super::#ident(#(#args_from_owned),*) == expected
             }
         }));
     }
+    let fn_ident = format_ident!("test_{}", ident);
+
+    tests_file.items.push(Item::Fn(parse_quote! {
+        #[test]
+        fn #fn_ident() {
+            #(#bindings)*
+            super::#ident(#(#args_from_owned),*);
+        }
+    }));
+}
+
+fn expr_into(ident: &Ident) -> syn::Expr {
+    syn::Expr::MethodCall(parse_quote! { #ident.into() })
+}
+
+fn new_pat_ident(ident: &Ident) -> syn::PatIdent {
+    syn::PatIdent {
+        attrs: vec![],
+        by_ref: None,
+        mutability: None,
+        ident: ident.clone(),
+        subpat: None,
+    }
+}
+
+fn ptr_to_ty_expr_pat(ident: &Ident, type_ptr: &syn::TypePtr) -> (syn::Type, syn::Expr, syn::Pat) {
+    let (ty, expr, pat) = match type_ptr.elem.as_ref() {
+        ty @ syn::Type::Path(_) => (
+            ty.clone(),
+            expr_into(ident),
+            // syn::Expr::Path(syn::ExprPath {
+            //     attrs: vec![],
+            //     qself: type_path.qself.clone(),
+            //     path: type_path.path.clone(),
+            // }),
+            syn::Pat::Ident(new_pat_ident(ident)),
+        ),
+        syn::Type::Ptr(type_ptr) => ptr_to_ty_expr_pat(ident, type_ptr),
+        _ => panic!("Unexpected type for {}: {:?}", ident, type_ptr),
+    };
+
+    (
+        ty,
+        syn::Expr::Reference(syn::ExprReference {
+            attrs: vec![],
+            and_token: token::And(Span::mixed_site()),
+            mutability: type_ptr.mutability,
+            expr: Box::new(expr),
+        }),
+        syn::Pat::Reference(syn::PatReference {
+            attrs: vec![],
+            and_token: token::And(Span::mixed_site()),
+            mutability: type_ptr.mutability,
+            pat: Box::new(pat),
+        }),
+    )
 }
