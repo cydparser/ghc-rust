ce1961435f0796e7701ab67b1d497911a96ca8b8
cydparser <cydparser@gmail.com>
2025-03-27 18:29:05 -0700

    Wraps static RHS in unsafe

--- generate/src/main.rs
+++ generate/src/main.rs
@@ -195,7 +195,7 @@ fn transform_tree(symbols: &InternalSymbols, syn_file: syn::File) -> Transformed
 
                             transformed.main_file.items.push(Item::Static(parse_quote! {
                                 #attr
-                                #vis static #mutability #ident: #ty = sys::#ident;
+                                #vis static #mutability #ident: #ty = unsafe { sys::#ident };
                             }));
                         }
                         fitem => panic!("Unexpected Item: {:#?}", fitem),
