3392974fb982132cf923b2ddd06a1f5eed21f0fd
cydparser <cydparser@gmail.com>
2025-03-27 17:56:59 -0700

    Combine MACHREGS_arch tests into single test

--- src/stg/mach_regs_for_host/tests.rs
+++ src/stg/mach_regs_for_host/tests.rs
@@ -7,68 +7,40 @@ fn test_eq_MACHREGS_NO_REGS() {
     assert_eq!(sys::MACHREGS_NO_REGS, super::MACHREGS_NO_REGS.into());
 }
 
-#[cfg(all(feature = "sys", target_arch = "aarch64"))]
+#[cfg(feature = "sys")]
 #[test]
-fn test_eq_MACHREGS_aarch64() {
+fn test_eq_MACHREGS_arch() {
+    #[cfg(target_arch = "aarch64")]
     assert_eq!(sys::MACHREGS_aarch64, super::MACHREGS_aarch64.into());
-}
 
-#[cfg(all(feature = "sys", target_arch = "arm"))]
-#[test]
-fn test_eq_MACHREGS_arm() {
+    #[cfg(target_arch = "arm")]
     assert_eq!(sys::MACHREGS_arm, super::MACHREGS_arm.into());
-}
 
-#[cfg(all(feature = "sys", target_arch = "loongarch64"))]
-#[test]
-fn test_eq_MACHREGS_loongarch64() {
+    #[cfg(target_arch = "loongarch64")]
     assert_eq!(
         sys::MACHREGS_loongarch64,
         super::MACHREGS_loongarch64.into()
     );
-}
 
-#[cfg(all(
-    feature = "sys",
-    any(target_arch = "powerpc", target_arch = "powerpc64")
-))]
-#[test]
-fn test_eq_MACHREGS_powerpc() {
+    #[cfg(any(target_arch = "powerpc", target_arch = "powerpc64"))]
     assert_eq!(sys::MACHREGS_powerpc, super::MACHREGS_powerpc.into());
-}
 
-#[cfg(all(feature = "sys", target_arch = "powerpc64"))]
-#[test]
-fn test_eq_MACHREGS_powerpc64() {
-    assert_eq!(sys::MACHREGS_powerpc64, super::MACHREGS_powerpc64.into());
-}
-
-#[cfg(all(feature = "sys", target_arch = "riscv64"))]
-#[test]
-fn test_eq_MACHREGS_riscv64() {
+    #[cfg(target_arch = "riscv64")]
     assert_eq!(sys::MACHREGS_riscv64, super::MACHREGS_riscv64.into());
-}
 
-#[cfg(all(feature = "sys", target_arch = "s390x"))]
-#[test]
-fn test_eq_MACHREGS_s390x() {
+    #[cfg(target_arch = "s390x")]
     assert_eq!(sys::MACHREGS_s390x, super::MACHREGS_s390x.into());
-}
 
-#[cfg(all(feature = "sys", target_arch = "wasm32"))]
-#[test]
-fn test_eq_MACHREGS_wasm32() {
+    #[cfg(target_arch = "wasm32")]
     assert_eq!(sys::MACHREGS_wasm32, super::MACHREGS_wasm32.into());
-}
 
-#[cfg(all(feature = "sys", target_arch = "x86_64"))]
-#[test]
-fn test_eq_MACHREGS_x86_64() {
+    #[cfg(target_arch = "x86_64")]
     assert_eq!(sys::MACHREGS_x86_64, super::MACHREGS_x86_64.into());
 }
 
-#[cfg(all(feature = "sys", target_os = "macos"))]
+#[cfg(feature = "sys")]
 #[test]
-fn test_eq_MACHREGS_darwin() {
+fn test_eq_MACHREGS_os() {
+    #[cfg(target_os = "macos")]
     assert_eq!(sys::MACHREGS_darwin, super::MACHREGS_darwin.into());
 }
