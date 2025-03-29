d2654cfbca5106dd5f4a2dc105b2ab3bc763202a
cydparser <cydparser@gmail.com>
2025-03-29 16:53:12 -0700

    Clean stg/regs

--- src/stg/regs.rs
+++ src/stg/regs.rs
@@ -1,16 +1,24 @@
-use crate::stg::types::{StgInt, StgPtr, StgWord, StgWord64};
-#[cfg(feature = "sys")]
-use ghc_rts_sys as sys;
-use libc::{clockid_t, pid_t, pthread_cond_t, pthread_key_t, pthread_mutex_t, pthread_t};
+use std::mem::transmute;
+
 #[cfg(test)]
 use quickcheck::{Arbitrary, Gen};
-use std::mem::transmute;
-#[cfg(feature = "tracing")]
-use tracing::instrument;
+
+use crate::{
+    rts::prof::ccs::CostCentreStack_,
+    rts::storage::{block::bdescr_, gc::nursery_, tso::StgTSO_},
+    stg::types::{
+        StgAddr, StgChar, StgDouble, StgFloat, StgFunPtr, StgInt, StgPtr, StgWord, StgWord128,
+        StgWord256, StgWord512, StgWord64,
+    },
+};
+#[cfg(feature = "sys")]
+use ghc_rts_sys as sys;
+
 #[cfg(test)]
 mod tests;
 
 #[repr(C)]
+#[derive(Clone)]
 pub(crate) struct StgFunTable {
     pub stgEagerBlackholeInfo: StgWord,
     pub stgGCEnter1: StgFunPtr,
@@ -29,20 +37,21 @@ impl Arbitrary for StgFunTable {
     fn arbitrary(g: &mut Gen) -> Self {
         StgFunTable {
             stgEagerBlackholeInfo: Arbitrary::arbitrary(g),
-            stgGCEnter1: Arbitrary::arbitrary(g),
-            stgGCFun: Arbitrary::arbitrary(g),
+            stgGCEnter1: None,
+            stgGCFun: None,
         }
     }
 }
 
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub(crate) union StgUnion {
-    pub w: ::core::mem::ManuallyDrop<StgWord>,
-    pub a: ::core::mem::ManuallyDrop<StgAddr>,
-    pub c: ::core::mem::ManuallyDrop<StgChar>,
-    pub f: ::core::mem::ManuallyDrop<StgFloat>,
-    pub i: ::core::mem::ManuallyDrop<StgInt>,
-    pub p: ::core::mem::ManuallyDrop<StgPtr>,
+    pub w: StgWord,
+    pub a: StgAddr,
+    pub c: StgChar,
+    pub f: StgFloat,
+    pub i: StgInt,
+    pub p: StgPtr,
 }
 
 #[cfg(feature = "sys")]
@@ -55,24 +64,18 @@ impl From<StgUnion> for sys::StgUnion {
 #[cfg(test)]
 impl Arbitrary for StgUnion {
     fn arbitrary(g: &mut Gen) -> Self {
-        match Arbitrary::arbitrary::<usize>(g) % 6usize {
+        match <usize as Arbitrary>::arbitrary(g) % 4_usize {
             0 => StgUnion {
                 w: Arbitrary::arbitrary(g),
             },
             1 => StgUnion {
-                a: Arbitrary::arbitrary(g),
-            },
-            2 => StgUnion {
                 c: Arbitrary::arbitrary(g),
             },
-            3 => StgUnion {
+            2 => StgUnion {
                 f: Arbitrary::arbitrary(g),
             },
-            4 => StgUnion {
+            3.. => StgUnion {
                 i: Arbitrary::arbitrary(g),
-            },
-            5 => StgUnion {
-                p: Arbitrary::arbitrary(g),
             },
         }
     }
@@ -138,65 +141,5 @@ pub struct StgRegTable {
 impl From<StgRegTable> for sys::StgRegTable {
     fn from(x: StgRegTable) -> Self {
         unsafe { transmute(x) }
-    }
-}
-
-#[cfg(test)]
-impl Arbitrary for StgRegTable {
-    fn arbitrary(g: &mut Gen) -> Self {
-        StgRegTable {
-            rR1: Arbitrary::arbitrary(g),
-            rR2: Arbitrary::arbitrary(g),
-            rR3: Arbitrary::arbitrary(g),
-            rR4: Arbitrary::arbitrary(g),
-            rR5: Arbitrary::arbitrary(g),
-            rR6: Arbitrary::arbitrary(g),
-            rR7: Arbitrary::arbitrary(g),
-            rR8: Arbitrary::arbitrary(g),
-            rR9: Arbitrary::arbitrary(g),
-            rR10: Arbitrary::arbitrary(g),
-            rF1: Arbitrary::arbitrary(g),
-            rF2: Arbitrary::arbitrary(g),
-            rF3: Arbitrary::arbitrary(g),
-            rF4: Arbitrary::arbitrary(g),
-            rF5: Arbitrary::arbitrary(g),
-            rF6: Arbitrary::arbitrary(g),
-            rD1: Arbitrary::arbitrary(g),
-            rD2: Arbitrary::arbitrary(g),
-            rD3: Arbitrary::arbitrary(g),
-            rD4: Arbitrary::arbitrary(g),
-            rD5: Arbitrary::arbitrary(g),
-            rD6: Arbitrary::arbitrary(g),
-            rXMM1: Arbitrary::arbitrary(g),
-            rXMM2: Arbitrary::arbitrary(g),
-            rXMM3: Arbitrary::arbitrary(g),
-            rXMM4: Arbitrary::arbitrary(g),
-            rXMM5: Arbitrary::arbitrary(g),
-            rXMM6: Arbitrary::arbitrary(g),
-            rYMM1: Arbitrary::arbitrary(g),
-            rYMM2: Arbitrary::arbitrary(g),
-            rYMM3: Arbitrary::arbitrary(g),
-            rYMM4: Arbitrary::arbitrary(g),
-            rYMM5: Arbitrary::arbitrary(g),
-            rYMM6: Arbitrary::arbitrary(g),
-            rZMM1: Arbitrary::arbitrary(g),
-            rZMM2: Arbitrary::arbitrary(g),
-            rZMM3: Arbitrary::arbitrary(g),
-            rZMM4: Arbitrary::arbitrary(g),
-            rZMM5: Arbitrary::arbitrary(g),
-            rZMM6: Arbitrary::arbitrary(g),
-            rL1: Arbitrary::arbitrary(g),
-            rSp: Arbitrary::arbitrary(g),
-            rSpLim: Arbitrary::arbitrary(g),
-            rHp: Arbitrary::arbitrary(g),
-            rHpLim: Arbitrary::arbitrary(g),
-            rCCCS: Arbitrary::arbitrary(g),
-            rCurrentTSO: Arbitrary::arbitrary(g),
-            rNursery: Arbitrary::arbitrary(g),
-            rCurrentNursery: Arbitrary::arbitrary(g),
-            rCurrentAlloc: Arbitrary::arbitrary(g),
-            rHpAlloc: Arbitrary::arbitrary(g),
-            rRet: Arbitrary::arbitrary(g),
-        }
     }
 }
--- src/stg/regs/tests.rs
+++ src/stg/regs/tests.rs
@@ -1,8 +1,9 @@
-use crate::stg::types::{StgInt, StgPtr, StgWord, StgWord64};
+use std::mem::size_of;
+
+use super::*;
 #[cfg(feature = "sys")]
 use ghc_rts_sys as sys;
-use quickcheck_macros::quickcheck;
-use std::mem::{size_of, transmute};
+
 #[cfg(feature = "sys")]
 #[test]
 fn test_size_of_StgFunTable() {
