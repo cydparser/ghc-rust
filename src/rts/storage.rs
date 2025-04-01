a4e83a3753dfcf694f0b96724c6f28cfa0046751
cydparser <cydparser@gmail.com>
2025-03-31 20:58:55 -0700

    Add statics to rts/storage

--- src/rts/storage.rs
+++ src/rts/storage.rs
@@ -1,3 +1,7 @@
+use std::ptr::null_mut;
+
+use crate::{rts::storage::gc::generation, stg::W_};
+
 pub mod block;
 pub mod closure_macros;
 pub mod closure_types;
@@ -8,3 +12,16 @@ pub mod heap;
 pub mod info_tables;
 pub mod m_block;
 pub mod tso;
+
+#[unsafe(no_mangle)]
+pub static mut generations: *mut generation = null_mut();
+
+#[unsafe(no_mangle)]
+pub static mut g0: *mut generation = null_mut();
+
+pub(crate) static mut oldest_gen: *mut generation = null_mut();
+
+pub(crate) static mut large_alloc_lim: W_ = 0; // TODO: Set default.
+
+#[unsafe(no_mangle)]
+pub static mut keepCAFs: bool = false;
