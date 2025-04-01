0d1f32a6a881102d97e0070d98f4107f25f60367
cydparser <cydparser@gmail.com>
2025-03-31 21:28:20 -0700

    Add include crate for generating C header

---
+++ include/build.rs
@@ -0,0 +1,16 @@
+use cbindgen;
+
+use std::{env, path::Path};
+
+fn main() {
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    let config = cbindgen::Config::from_file(manifest_dir.join("cbindgen.toml")).unwrap();
+
+    cbindgen::Builder::new()
+        .with_config(config)
+        .with_crate(manifest_dir)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("Rts.h");
+}
