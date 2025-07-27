6ab988d5269e30f256e6d97d6f28f303f728c2b3
cydparser <cydparser@gmail.com>
2025-07-27 15:17:14 -0700

    Clean rts/adjustor

--- rts/src/rts/adjustor.rs
+++ rts/src/rts/adjustor.rs
@@ -1,15 +1,6 @@
-use crate::stg::types::{StgInt, StgPtr, StgWord, StgWord64};
-#[cfg(test)]
-use crate::utils::test::{Arbitrary, Gen, HasReferences};
-#[cfg(feature = "sys")]
-use ghc_rts_sys as sys;
-use libc::{clockid_t, pid_t, pthread_cond_t, pthread_key_t, pthread_mutex_t, pthread_t};
-use std::ffi::{c_char, c_int, c_uint, c_void};
-use std::mem::transmute;
-use std::ptr::{null, null_mut};
-use std::slice;
-#[cfg(feature = "tracing")]
-use tracing::instrument;
+use crate::prelude::*;
+use crate::stg::types::{StgFunPtr, StgStablePtr};
+
 #[cfg(test)]
 mod tests;
 
--- rts/src/rts/adjustor/tests.rs
+++ rts/src/rts/adjustor/tests.rs
@@ -1,17 +1,15 @@
 use super::*;
-use crate::stg::types::{StgInt, StgPtr, StgWord, StgWord64};
-use crate::utils::test::*;
-#[cfg(feature = "sys")]
-use ghc_rts_sys as sys;
-use quickcheck_macros::quickcheck;
-use std::ffi::{c_char, c_int, c_uint, c_void};
-use std::mem::transmute;
-use std::ptr::{null, null_mut};
+use crate::stg::types::{StgFunPtr, StgStablePtr};
+use std::ffi::CString;
+
 #[cfg(feature = "sys")]
 #[quickcheck]
-fn equivalent_createAdjustor(hptr: StgStablePtr, wptr: StgFunPtr, typeString: c_char) -> bool {
-    let expected = unsafe { sys::createAdjustor(hptr, wptr, &mut typeString) };
-    let actual = unsafe { createAdjustor(hptr, wptr, &mut typeString) };
+#[ignore]
+fn equivalent_createAdjustor(typeString: CString) -> bool {
+    let hptr: StgStablePtr = Default::default();
+    let wptr: StgFunPtr = Default::default();
+    let expected = unsafe { sys::createAdjustor(hptr, wptr, typeString.as_ptr() as *mut c_char) };
+    let actual = unsafe { createAdjustor(hptr, wptr, typeString.as_ptr() as *mut c_char) };
     actual == expected
 }
 
@@ -20,15 +18,16 @@ fn equivalent_createAdjustor(hptr: StgStablePtr, wptr: StgFunPtr, typeString: c_
 fn test_createAdjustor() {
     let hptr = Default::default();
     let wptr = Default::default();
-    let mut typeString = null_mut();
-    unsafe { createAdjustor(hptr, wptr, &mut typeString) };
+    #[expect(unused_mut)]
+    let mut typeString = CString::from(c"TODO");
+    unsafe { createAdjustor(hptr, wptr, typeString.as_ptr() as *mut c_char) };
     todo!("assert")
 }
 
 #[test]
 #[ignore]
 fn test_freeHaskellFunctionPtr() {
-    let mut ptr = null_mut();
-    unsafe { freeHaskellFunctionPtr(&mut ptr) };
+    let ptr = null_mut(); // TODO
+    unsafe { freeHaskellFunctionPtr(ptr) };
     todo!("assert")
 }
