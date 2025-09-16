57de199675da13be59544c1e7d263b92df42ec8a
cydparser <cydparser@gmail.com>
2025-09-15 17:11:03 -0700

    Add capability

---
+++ rts/src/capability.rs
@@ -0,0 +1,18 @@
+use crate::prelude::*;
+
+/// - GHC_PLACES: {libraries, testsuite}
+pub type Capability = Capability_;
+
+/// cbindgen:no-export
+#[repr(C)]
+#[derive(Debug)]
+pub struct Capability_ {
+    _unused: [u8; 0],
+}
+
+#[cfg(feature = "sys")]
+impl From<Capability_> for sys::Capability_ {
+    fn from(x: Capability_) -> Self {
+        unsafe { transmute(x) }
+    }
+}
