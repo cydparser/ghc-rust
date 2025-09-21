12f23d5282faf637d705f1bd34c8c6bd5e5ae120
cydparser <cydparser@gmail.com>
2025-09-21 16:18:21 -0700

    Modify stg/types

--- rts/src/stg/types.rs
+++ rts/src/stg/types.rs
@@ -1,39 +1,59 @@
 use crate::prelude::*;
 
+// This module should define types *only*, all beginning with "Stg".
+//
+// Specifically:
+//
+//     StgInt8,  16, 32, 64
+//     StgWord8, 16, 32, 64
+//     StgChar, StgFloat, StgDouble
+//
+//     ***** All the same size (i.e. sizeof(void *)): *****
+//     StgPtr                  Basic pointer type
+//     StgWord                 Unit of heap allocation
+//     StgInt                  Signed version of StgWord
+//     StgAddr                 Generic address type
+//
+//     StgBool, StgVoid, StgPtr, StgOffset,
+//     StgCode, StgStablePtr, StgFunPtr,
+//     StgUnion.
+
 #[cfg(test)]
 mod tests;
 
 pub(crate) const STG_INT8_MIN: i32 = -128;
 
-/// - GHC_PLACES: {testsuite}
-pub const STG_INT8_MAX: u32 = 127;
+// First, platform-dependent definitions of size-specific integers.
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_WORD8_MAX: u32 = 255;
-
-pub(crate) const STG_INT16_MIN: i32 = -32768;
+pub const STG_INT8_MAX: u32 = i8::MAX as u32;
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_INT16_MAX: u32 = 32767;
+pub const STG_WORD8_MAX: u32 = u8::MAX as u32;
+
+pub(crate) const STG_INT16_MIN: i32 = i16::MIN as i32;
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_WORD16_MAX: u32 = 65535;
-
-pub(crate) const STG_INT32_MIN: i32 = -2147483648;
+pub const STG_INT16_MAX: u32 = i16::MAX as u32;
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_INT32_MAX: u32 = 2147483647;
+pub const STG_WORD16_MAX: u32 = u16::MAX as u32;
+
+pub(crate) const STG_INT32_MIN: i32 = i32::MIN;
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_WORD32_MAX: u32 = 4294967295;
-
-pub(crate) const STG_INT64_MIN: i64 = -9223372036854775808;
+pub const STG_INT32_MAX: u32 = i32::MAX as u32;
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_INT64_MAX: u64 = 9223372036854775807;
+pub const STG_WORD32_MAX: u32 = u32::MAX;
+
+pub(crate) const STG_INT64_MIN: i64 = i64::MIN;
 
 /// - GHC_PLACES: {testsuite}
-pub const STG_WORD64_MAX: i32 = -1;
+pub const STG_INT64_MAX: u64 = i64::MAX as u64;
+
+/// - GHC_PLACES: {testsuite}
+pub const STG_WORD64_MAX: u64 = u64::MAX;
 
 pub(crate) const STG_INT_MIN: i64 = -9223372036854775808;
 
@@ -67,10 +87,10 @@ pub type StgInt64 = i64;
 /// - GHC_PLACES: {libraries, testsuite}
 pub type StgWord64 = u64;
 
+// TODO(rust): pub type StgWord128 = u128;
 /// cbindgen:no-export
 #[repr(C)]
-#[derive(Debug)]
-#[cfg_attr(test, derive(Clone))]
+#[derive(Clone, Copy, Debug)]
 pub struct StgWord128 {
     h: StgWord64,
     l: StgWord64,
@@ -145,15 +165,40 @@ impl Arbitrary for StgWord512 {
     }
 }
 
+// Stg{Int,Word} are defined such that they have the exact same size as a void pointer.
+
+const _: () = {
+    if cfg!(not(any(
+        target_pointer_width = "64",
+        target_pointer_width = "32"
+    ))) {
+        panic!("GHC untested on this architecture: sizeof(void *) != 4 or 8");
+    }
+};
+
 /// - GHC_PLACES: {testsuite}
+#[cfg(target_pointer_width = "64")]
 pub type StgInt = i64;
+#[cfg(target_pointer_width = "32")]
+pub type StgInt = i32;
 
 /// - GHC_PLACES: {libraries, testsuite}
+#[cfg(target_pointer_width = "64")]
 pub type StgWord = u64;
+#[cfg(target_pointer_width = "32")]
+pub type StgWord = u32;
 
+#[cfg(target_pointer_width = "64")]
 pub(crate) type StgHalfInt = i32;
+#[cfg(target_pointer_width = "32")]
+pub(crate) type StgHalfInt = i16;
 
+#[cfg(target_pointer_width = "64")]
 pub(crate) type StgHalfWord = u32;
+#[cfg(target_pointer_width = "32")]
+pub(crate) type StgHalfWord = u16;
+
+// Other commonly-used STG datatypes.
 
 pub(crate) type StgAddr = *mut c_void;
 
@@ -182,6 +227,19 @@ pub(crate) type StgCode = StgWord8;
 pub type StgStablePtr = *mut c_void;
 
 pub(crate) type StgByteArray = *mut StgWord8;
+
+// Types for generated C functions when compiling via C.
+//
+// The C functions take no arguments, and return a pointer to the next
+// function to be called use: Ptr to Fun that returns a Ptr to Fun
+// which returns Ptr to void
+//
+// Note: Neither StgFunPtr not StgFun is quite right (that is,
+// StgFunPtr != StgFun*).  So, the functions we define all have type
+// StgFun but we always have to cast them to StgFunPtr when we assign
+// them to something.
+// The only way round this would be to write a recursive type but
+// C only allows that if you're defining a struct or union.
 
 pub(crate) type StgFunPtr =
     Option<unsafe extern "C" fn() -> Option<unsafe extern "C" fn() -> *mut c_void>>;
