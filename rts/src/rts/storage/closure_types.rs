ae268d50260a98ae56e2c95bf862124596ba6e65
cydparser <cydparser@gmail.com>
2025-10-06 16:24:24 -0700

    Convert closure_types consts to enum

--- rts/src/rts/storage/closure_types.rs
+++ rts/src/rts/storage/closure_types.rs
@@ -2,199 +2,75 @@
 mod tests;
 
 /// - GHC_PLACES: {compiler, libraries}
-pub const INVALID_OBJECT: u32 = 0;
-
-/// - GHC_PLACES: {compiler, libraries}
-pub const CONSTR: u32 = 1;
-
-/// - GHC_PLACES: {compiler}
-pub const CONSTR_1_0: u32 = 2;
-
-/// - GHC_PLACES: {compiler, testsuite}
-pub const CONSTR_0_1: u32 = 3;
-
-/// - GHC_PLACES: {compiler}
-pub const CONSTR_2_0: u32 = 4;
-
-/// - GHC_PLACES: {compiler}
-pub const CONSTR_1_1: u32 = 5;
-
-/// - GHC_PLACES: {compiler}
-pub const CONSTR_0_2: u32 = 6;
-
-/// - GHC_PLACES: {compiler}
-pub const CONSTR_NOCAF: u32 = 7;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN: u32 = 8;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN_1_0: u32 = 9;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN_0_1: u32 = 10;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN_2_0: u32 = 11;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN_1_1: u32 = 12;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN_0_2: u32 = 13;
-
-/// - GHC_PLACES: {compiler}
-pub const FUN_STATIC: u32 = 14;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK: u32 = 15;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_1_0: u32 = 16;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_0_1: u32 = 17;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_2_0: u32 = 18;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_1_1: u32 = 19;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_0_2: u32 = 20;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_STATIC: u32 = 21;
-
-/// - GHC_PLACES: {compiler}
-pub const THUNK_SELECTOR: u32 = 22;
-
-/// - GHC_PLACES: {compiler}
-pub const BCO: u32 = 23;
-
-/// - GHC_PLACES: {compiler}
-pub const AP: u32 = 24;
-
-/// - GHC_PLACES: {compiler}
-pub const PAP: u32 = 25;
-
-/// - GHC_PLACES: {compiler}
-pub const AP_STACK: u32 = 26;
-
-/// - GHC_PLACES: {compiler}
-pub const IND: u32 = 27;
-
-/// - GHC_PLACES: {compiler}
-pub const IND_STATIC: u32 = 28;
-
-/// - GHC_PLACES: {compiler}
-pub const RET_BCO: u32 = 29;
-
-/// - GHC_PLACES: {compiler}
-pub const RET_SMALL: u32 = 30;
-
-/// - GHC_PLACES: {compiler, testsuite}
-pub const RET_BIG: u32 = 31;
-
-/// - GHC_PLACES: {compiler}
-pub const RET_FUN: u32 = 32;
-
-/// - GHC_PLACES: {compiler}
-pub const UPDATE_FRAME: u32 = 33;
-
-/// - GHC_PLACES: {compiler}
-pub const CATCH_FRAME: u32 = 34;
-
-/// - GHC_PLACES: {compiler, libraries}
-pub const UNDERFLOW_FRAME: u32 = 35;
-
-/// - GHC_PLACES: {compiler}
-pub const STOP_FRAME: u32 = 36;
-
-/// - GHC_PLACES: {compiler}
-pub const BLOCKING_QUEUE: u32 = 37;
-
-/// - GHC_PLACES: {compiler, libraries}
-pub const BLACKHOLE: u32 = 38;
-
-/// - GHC_PLACES: {compiler}
-pub const MVAR_CLEAN: u32 = 39;
-
-/// - GHC_PLACES: {compiler}
-pub const MVAR_DIRTY: u32 = 40;
-
-/// - GHC_PLACES: {compiler}
-pub const TVAR: u32 = 41;
-
-/// - GHC_PLACES: {compiler}
-pub const ARR_WORDS: u32 = 42;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_ARR_PTRS_CLEAN: u32 = 43;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_ARR_PTRS_DIRTY: u32 = 44;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_ARR_PTRS_FROZEN_DIRTY: u32 = 45;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_ARR_PTRS_FROZEN_CLEAN: u32 = 46;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_VAR_CLEAN: u32 = 47;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_VAR_DIRTY: u32 = 48;
-
-/// - GHC_PLACES: {compiler, testsuite}
-pub const WEAK: u32 = 49;
-
-/// - GHC_PLACES: {compiler}
-pub const PRIM: u32 = 50;
-
-/// - GHC_PLACES: {compiler}
-pub const MUT_PRIM: u32 = 51;
-
-/// - GHC_PLACES: {compiler, libraries, testsuite}
-pub const TSO: u32 = 52;
-
-/// - GHC_PLACES: {compiler, libraries, testsuite}
-pub const STACK: u32 = 53;
-
-/// - GHC_PLACES: {compiler}
-pub const TREC_CHUNK: u32 = 54;
-
-/// - GHC_PLACES: {compiler}
-pub const ATOMICALLY_FRAME: u32 = 55;
-
-/// - GHC_PLACES: {compiler}
-pub const CATCH_RETRY_FRAME: u32 = 56;
-
-/// - GHC_PLACES: {compiler}
-pub const CATCH_STM_FRAME: u32 = 57;
-
-/// - GHC_PLACES: {compiler}
-pub const WHITEHOLE: u32 = 58;
-
-/// - GHC_PLACES: {compiler}
-pub const SMALL_MUT_ARR_PTRS_CLEAN: u32 = 59;
-
-/// - GHC_PLACES: {compiler}
-pub const SMALL_MUT_ARR_PTRS_DIRTY: u32 = 60;
-
-/// - GHC_PLACES: {compiler}
-pub const SMALL_MUT_ARR_PTRS_FROZEN_DIRTY: u32 = 61;
-
-/// - GHC_PLACES: {compiler}
-pub const SMALL_MUT_ARR_PTRS_FROZEN_CLEAN: u32 = 62;
-
-/// - GHC_PLACES: {compiler}
-pub const COMPACT_NFDATA: u32 = 63;
-
-/// - GHC_PLACES: {compiler}
-pub const CONTINUATION: u32 = 64;
-
-/// - GHC_PLACES: {compiler}
-pub const N_CLOSURE_TYPES: u32 = 65;
+/// libraries/ghc-internal/src/GHC/Internal/ClosureTypes.hs
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ClosureType {
+    INVALID_OBJECT = 0,
+    CONSTR = 1,
+    CONSTR_1_0 = 2,
+    CONSTR_0_1 = 3,
+    CONSTR_2_0 = 4,
+    CONSTR_1_1 = 5,
+    CONSTR_0_2 = 6,
+    CONSTR_NOCAF = 7,
+    FUN = 8,
+    FUN_1_0 = 9,
+    FUN_0_1 = 10,
+    FUN_2_0 = 11,
+    FUN_1_1 = 12,
+    FUN_0_2 = 13,
+    FUN_STATIC = 14,
+    THUNK = 15,
+    THUNK_1_0 = 16,
+    THUNK_0_1 = 17,
+    THUNK_2_0 = 18,
+    THUNK_1_1 = 19,
+    THUNK_0_2 = 20,
+    THUNK_STATIC = 21,
+    THUNK_SELECTOR = 22,
+    BCO = 23,
+    AP = 24,
+    PAP = 25,
+    AP_STACK = 26,
+    IND = 27,
+    IND_STATIC = 28,
+    RET_BCO = 29,
+    RET_SMALL = 30,
+    RET_BIG = 31,
+    RET_FUN = 32,
+    UPDATE_FRAME = 33,
+    CATCH_FRAME = 34,
+    UNDERFLOW_FRAME = 35,
+    STOP_FRAME = 36,
+    BLOCKING_QUEUE = 37,
+    BLACKHOLE = 38,
+    MVAR_CLEAN = 39,
+    MVAR_DIRTY = 40,
+    TVAR = 41,
+    ARR_WORDS = 42,
+    MUT_ARR_PTRS_CLEAN = 43,
+    MUT_ARR_PTRS_DIRTY = 44,
+    MUT_ARR_PTRS_FROZEN_DIRTY = 45,
+    MUT_ARR_PTRS_FROZEN_CLEAN = 46,
+    MUT_VAR_CLEAN = 47,
+    MUT_VAR_DIRTY = 48,
+    WEAK = 49,
+    PRIM = 50,
+    MUT_PRIM = 51,
+    TSO = 52,
+    STACK = 53,
+    TREC_CHUNK = 54,
+    ATOMICALLY_FRAME = 55,
+    CATCH_RETRY_FRAME = 56,
+    CATCH_STM_FRAME = 57,
+    WHITEHOLE = 58,
+    SMALL_MUT_ARR_PTRS_CLEAN = 59,
+    SMALL_MUT_ARR_PTRS_DIRTY = 60,
+    SMALL_MUT_ARR_PTRS_FROZEN_DIRTY = 61,
+    SMALL_MUT_ARR_PTRS_FROZEN_CLEAN = 62,
+    COMPACT_NFDATA = 63,
+    CONTINUATION = 64,
+    ANN_FRAME = 65,
+    N_CLOSURE_TYPES = 66,
+}
--- rts/src/rts/storage/closure_types/tests.rs
+++ rts/src/rts/storage/closure_types/tests.rs
@@ -5,367 +5,388 @@ use crate::prelude::*;
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_INVALID_OBJECT() {
-    assert_eq!(sys::INVALID_OBJECT, INVALID_OBJECT);
+    assert_eq!(sys::INVALID_OBJECT, ClosureType::INVALID_OBJECT as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR() {
-    assert_eq!(sys::CONSTR, CONSTR);
+    assert_eq!(sys::CONSTR, ClosureType::CONSTR as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR_1_0() {
-    assert_eq!(sys::CONSTR_1_0, CONSTR_1_0);
+    assert_eq!(sys::CONSTR_1_0, ClosureType::CONSTR_1_0 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR_0_1() {
-    assert_eq!(sys::CONSTR_0_1, CONSTR_0_1);
+    assert_eq!(sys::CONSTR_0_1, ClosureType::CONSTR_0_1 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR_2_0() {
-    assert_eq!(sys::CONSTR_2_0, CONSTR_2_0);
+    assert_eq!(sys::CONSTR_2_0, ClosureType::CONSTR_2_0 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR_1_1() {
-    assert_eq!(sys::CONSTR_1_1, CONSTR_1_1);
+    assert_eq!(sys::CONSTR_1_1, ClosureType::CONSTR_1_1 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR_0_2() {
-    assert_eq!(sys::CONSTR_0_2, CONSTR_0_2);
+    assert_eq!(sys::CONSTR_0_2, ClosureType::CONSTR_0_2 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONSTR_NOCAF() {
-    assert_eq!(sys::CONSTR_NOCAF, CONSTR_NOCAF);
+    assert_eq!(sys::CONSTR_NOCAF, ClosureType::CONSTR_NOCAF as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN() {
-    assert_eq!(sys::FUN, FUN);
+    assert_eq!(sys::FUN, ClosureType::FUN as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN_1_0() {
-    assert_eq!(sys::FUN_1_0, FUN_1_0);
+    assert_eq!(sys::FUN_1_0, ClosureType::FUN_1_0 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN_0_1() {
-    assert_eq!(sys::FUN_0_1, FUN_0_1);
+    assert_eq!(sys::FUN_0_1, ClosureType::FUN_0_1 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN_2_0() {
-    assert_eq!(sys::FUN_2_0, FUN_2_0);
+    assert_eq!(sys::FUN_2_0, ClosureType::FUN_2_0 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN_1_1() {
-    assert_eq!(sys::FUN_1_1, FUN_1_1);
+    assert_eq!(sys::FUN_1_1, ClosureType::FUN_1_1 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN_0_2() {
-    assert_eq!(sys::FUN_0_2, FUN_0_2);
+    assert_eq!(sys::FUN_0_2, ClosureType::FUN_0_2 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_FUN_STATIC() {
-    assert_eq!(sys::FUN_STATIC, FUN_STATIC);
+    assert_eq!(sys::FUN_STATIC, ClosureType::FUN_STATIC as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK() {
-    assert_eq!(sys::THUNK, THUNK);
+    assert_eq!(sys::THUNK, ClosureType::THUNK as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_1_0() {
-    assert_eq!(sys::THUNK_1_0, THUNK_1_0);
+    assert_eq!(sys::THUNK_1_0, ClosureType::THUNK_1_0 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_0_1() {
-    assert_eq!(sys::THUNK_0_1, THUNK_0_1);
+    assert_eq!(sys::THUNK_0_1, ClosureType::THUNK_0_1 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_2_0() {
-    assert_eq!(sys::THUNK_2_0, THUNK_2_0);
+    assert_eq!(sys::THUNK_2_0, ClosureType::THUNK_2_0 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_1_1() {
-    assert_eq!(sys::THUNK_1_1, THUNK_1_1);
+    assert_eq!(sys::THUNK_1_1, ClosureType::THUNK_1_1 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_0_2() {
-    assert_eq!(sys::THUNK_0_2, THUNK_0_2);
+    assert_eq!(sys::THUNK_0_2, ClosureType::THUNK_0_2 as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_STATIC() {
-    assert_eq!(sys::THUNK_STATIC, THUNK_STATIC);
+    assert_eq!(sys::THUNK_STATIC, ClosureType::THUNK_STATIC as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_THUNK_SELECTOR() {
-    assert_eq!(sys::THUNK_SELECTOR, THUNK_SELECTOR);
+    assert_eq!(sys::THUNK_SELECTOR, ClosureType::THUNK_SELECTOR as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_BCO() {
-    assert_eq!(sys::BCO, BCO);
+    assert_eq!(sys::BCO, ClosureType::BCO as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_AP() {
-    assert_eq!(sys::AP, AP);
+    assert_eq!(sys::AP, ClosureType::AP as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_PAP() {
-    assert_eq!(sys::PAP, PAP);
+    assert_eq!(sys::PAP, ClosureType::PAP as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_AP_STACK() {
-    assert_eq!(sys::AP_STACK, AP_STACK);
+    assert_eq!(sys::AP_STACK, ClosureType::AP_STACK as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_IND() {
-    assert_eq!(sys::IND, IND);
+    assert_eq!(sys::IND, ClosureType::IND as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_IND_STATIC() {
-    assert_eq!(sys::IND_STATIC, IND_STATIC);
+    assert_eq!(sys::IND_STATIC, ClosureType::IND_STATIC as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_RET_BCO() {
-    assert_eq!(sys::RET_BCO, RET_BCO);
+    assert_eq!(sys::RET_BCO, ClosureType::RET_BCO as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_RET_SMALL() {
-    assert_eq!(sys::RET_SMALL, RET_SMALL);
+    assert_eq!(sys::RET_SMALL, ClosureType::RET_SMALL as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_RET_BIG() {
-    assert_eq!(sys::RET_BIG, RET_BIG);
+    assert_eq!(sys::RET_BIG, ClosureType::RET_BIG as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_RET_FUN() {
-    assert_eq!(sys::RET_FUN, RET_FUN);
+    assert_eq!(sys::RET_FUN, ClosureType::RET_FUN as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_UPDATE_FRAME() {
-    assert_eq!(sys::UPDATE_FRAME, UPDATE_FRAME);
+    assert_eq!(sys::UPDATE_FRAME, ClosureType::UPDATE_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CATCH_FRAME() {
-    assert_eq!(sys::CATCH_FRAME, CATCH_FRAME);
+    assert_eq!(sys::CATCH_FRAME, ClosureType::CATCH_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_UNDERFLOW_FRAME() {
-    assert_eq!(sys::UNDERFLOW_FRAME, UNDERFLOW_FRAME);
+    assert_eq!(sys::UNDERFLOW_FRAME, ClosureType::UNDERFLOW_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_STOP_FRAME() {
-    assert_eq!(sys::STOP_FRAME, STOP_FRAME);
+    assert_eq!(sys::STOP_FRAME, ClosureType::STOP_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_BLOCKING_QUEUE() {
-    assert_eq!(sys::BLOCKING_QUEUE, BLOCKING_QUEUE);
+    assert_eq!(sys::BLOCKING_QUEUE, ClosureType::BLOCKING_QUEUE as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_BLACKHOLE() {
-    assert_eq!(sys::BLACKHOLE, BLACKHOLE);
+    assert_eq!(sys::BLACKHOLE, ClosureType::BLACKHOLE as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MVAR_CLEAN() {
-    assert_eq!(sys::MVAR_CLEAN, MVAR_CLEAN);
+    assert_eq!(sys::MVAR_CLEAN, ClosureType::MVAR_CLEAN as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MVAR_DIRTY() {
-    assert_eq!(sys::MVAR_DIRTY, MVAR_DIRTY);
+    assert_eq!(sys::MVAR_DIRTY, ClosureType::MVAR_DIRTY as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_TVAR() {
-    assert_eq!(sys::TVAR, TVAR);
+    assert_eq!(sys::TVAR, ClosureType::TVAR as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_ARR_WORDS() {
-    assert_eq!(sys::ARR_WORDS, ARR_WORDS);
+    assert_eq!(sys::ARR_WORDS, ClosureType::ARR_WORDS as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_ARR_PTRS_CLEAN() {
-    assert_eq!(sys::MUT_ARR_PTRS_CLEAN, MUT_ARR_PTRS_CLEAN);
+    assert_eq!(
+        sys::MUT_ARR_PTRS_CLEAN,
+        ClosureType::MUT_ARR_PTRS_CLEAN as u32
+    );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_ARR_PTRS_DIRTY() {
-    assert_eq!(sys::MUT_ARR_PTRS_DIRTY, MUT_ARR_PTRS_DIRTY);
+    assert_eq!(
+        sys::MUT_ARR_PTRS_DIRTY,
+        ClosureType::MUT_ARR_PTRS_DIRTY as u32
+    );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_ARR_PTRS_FROZEN_DIRTY() {
-    assert_eq!(sys::MUT_ARR_PTRS_FROZEN_DIRTY, MUT_ARR_PTRS_FROZEN_DIRTY);
+    assert_eq!(
+        sys::MUT_ARR_PTRS_FROZEN_DIRTY,
+        ClosureType::MUT_ARR_PTRS_FROZEN_DIRTY as u32
+    );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_ARR_PTRS_FROZEN_CLEAN() {
-    assert_eq!(sys::MUT_ARR_PTRS_FROZEN_CLEAN, MUT_ARR_PTRS_FROZEN_CLEAN);
+    assert_eq!(
+        sys::MUT_ARR_PTRS_FROZEN_CLEAN,
+        ClosureType::MUT_ARR_PTRS_FROZEN_CLEAN as u32
+    );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_VAR_CLEAN() {
-    assert_eq!(sys::MUT_VAR_CLEAN, MUT_VAR_CLEAN);
+    assert_eq!(sys::MUT_VAR_CLEAN, ClosureType::MUT_VAR_CLEAN as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_VAR_DIRTY() {
-    assert_eq!(sys::MUT_VAR_DIRTY, MUT_VAR_DIRTY);
+    assert_eq!(sys::MUT_VAR_DIRTY, ClosureType::MUT_VAR_DIRTY as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_WEAK() {
-    assert_eq!(sys::WEAK, WEAK);
+    assert_eq!(sys::WEAK, ClosureType::WEAK as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_PRIM() {
-    assert_eq!(sys::PRIM, PRIM);
+    assert_eq!(sys::PRIM, ClosureType::PRIM as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_MUT_PRIM() {
-    assert_eq!(sys::MUT_PRIM, MUT_PRIM);
+    assert_eq!(sys::MUT_PRIM, ClosureType::MUT_PRIM as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_TSO() {
-    assert_eq!(sys::TSO, TSO);
+    assert_eq!(sys::TSO, ClosureType::TSO as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_STACK() {
-    assert_eq!(sys::STACK, STACK);
+    assert_eq!(sys::STACK, ClosureType::STACK as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_TREC_CHUNK() {
-    assert_eq!(sys::TREC_CHUNK, TREC_CHUNK);
+    assert_eq!(sys::TREC_CHUNK, ClosureType::TREC_CHUNK as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_ATOMICALLY_FRAME() {
-    assert_eq!(sys::ATOMICALLY_FRAME, ATOMICALLY_FRAME);
+    assert_eq!(sys::ATOMICALLY_FRAME, ClosureType::ATOMICALLY_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CATCH_RETRY_FRAME() {
-    assert_eq!(sys::CATCH_RETRY_FRAME, CATCH_RETRY_FRAME);
+    assert_eq!(
+        sys::CATCH_RETRY_FRAME,
+        ClosureType::CATCH_RETRY_FRAME as u32
+    );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CATCH_STM_FRAME() {
-    assert_eq!(sys::CATCH_STM_FRAME, CATCH_STM_FRAME);
+    assert_eq!(sys::CATCH_STM_FRAME, ClosureType::CATCH_STM_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_WHITEHOLE() {
-    assert_eq!(sys::WHITEHOLE, WHITEHOLE);
+    assert_eq!(sys::WHITEHOLE, ClosureType::WHITEHOLE as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_SMALL_MUT_ARR_PTRS_CLEAN() {
-    assert_eq!(sys::SMALL_MUT_ARR_PTRS_CLEAN, SMALL_MUT_ARR_PTRS_CLEAN);
+    assert_eq!(
+        sys::SMALL_MUT_ARR_PTRS_CLEAN,
+        ClosureType::SMALL_MUT_ARR_PTRS_CLEAN as u32
+    );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_SMALL_MUT_ARR_PTRS_DIRTY() {
-    assert_eq!(sys::SMALL_MUT_ARR_PTRS_DIRTY, SMALL_MUT_ARR_PTRS_DIRTY);
+    assert_eq!(
+        sys::SMALL_MUT_ARR_PTRS_DIRTY,
+        ClosureType::SMALL_MUT_ARR_PTRS_DIRTY as u32
+    );
 }
 
 #[cfg(feature = "sys")]
@@ -373,7 +394,7 @@ fn sys_eq_SMALL_MUT_ARR_PTRS_DIRTY() {
 fn sys_eq_SMALL_MUT_ARR_PTRS_FROZEN_DIRTY() {
     assert_eq!(
         sys::SMALL_MUT_ARR_PTRS_FROZEN_DIRTY,
-        SMALL_MUT_ARR_PTRS_FROZEN_DIRTY
+        ClosureType::SMALL_MUT_ARR_PTRS_FROZEN_DIRTY as u32
     );
 }
 
@@ -382,24 +403,30 @@ fn sys_eq_SMALL_MUT_ARR_PTRS_FROZEN_DIRTY() {
 fn sys_eq_SMALL_MUT_ARR_PTRS_FROZEN_CLEAN() {
     assert_eq!(
         sys::SMALL_MUT_ARR_PTRS_FROZEN_CLEAN,
-        SMALL_MUT_ARR_PTRS_FROZEN_CLEAN
+        ClosureType::SMALL_MUT_ARR_PTRS_FROZEN_CLEAN as u32
     );
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_COMPACT_NFDATA() {
-    assert_eq!(sys::COMPACT_NFDATA, COMPACT_NFDATA);
+    assert_eq!(sys::COMPACT_NFDATA, ClosureType::COMPACT_NFDATA as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_CONTINUATION() {
-    assert_eq!(sys::CONTINUATION, CONTINUATION);
+    assert_eq!(sys::CONTINUATION, ClosureType::CONTINUATION as u32);
+}
+
+#[cfg(feature = "sys")]
+#[test]
+fn sys_eq_ANN_FRAME() {
+    assert_eq!(sys::ANN_FRAME, ClosureType::ANN_FRAME as u32);
 }
 
 #[cfg(feature = "sys")]
 #[test]
 fn sys_eq_N_CLOSURE_TYPES() {
-    assert_eq!(sys::N_CLOSURE_TYPES, N_CLOSURE_TYPES);
+    assert_eq!(sys::N_CLOSURE_TYPES, ClosureType::N_CLOSURE_TYPES as u32);
 }
