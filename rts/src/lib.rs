a7e98d9a60a533b88cae08af6cb3ced709d80b3e
cydparser <cydparser@gmail.com>
2025-11-16 14:36:01 -0800

    Move rts FFI into ffi/

--- rts/src/lib.rs
+++ rts/src/lib.rs
@@ -1,20 +1,10 @@
-#![allow(clippy::missing_transmute_annotations)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
 // TODO(rust): Remove after finishing port.
 #![allow(clippy::missing_safety_doc)]
 #![allow(dead_code)]
 #![cfg_attr(not(feature = "sys"), expect(unused_variables))]
 
+mod capability;
 mod prelude;
 mod utils;
 
-// C-API
-pub mod hs_ffi;
-pub mod mach_deps;
-pub mod rts;
-pub mod rts_api;
-pub mod stg;
-
-mod capability;
+pub mod ffi;
