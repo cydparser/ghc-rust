c3a4694d878d468780c02caff3c6f25b6610655b
cydparser <cydparser@gmail.com>
2025-11-30 16:40:30 -0800

    Relax warning only for ffi modules

--- rts/src/ffi.rs
+++ rts/src/ffi.rs
@@ -1,7 +1,10 @@
-#![allow(clippy::missing_transmute_annotations)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+// TODO(rust): Remove after finishing port.
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::undocumented_unsafe_blocks)]
+#![cfg_attr(not(feature = "sys"), expect(unused_variables))]
 
 pub mod hs_ffi;
 pub mod mach_deps;
--- rts/src/lib.rs
+++ rts/src/lib.rs
@@ -1,7 +1,4 @@
-// TODO(rust): Remove after finishing port.
-#![allow(clippy::missing_safety_doc)]
-#![allow(dead_code)]
-#![cfg_attr(not(feature = "sys"), expect(unused_variables))]
+#![warn(clippy::undocumented_unsafe_blocks)]
 
 mod capability;
 mod prelude;
