778f0f4d748d1a74beb83dfecc67398b36cef287
cydparser <cydparser@gmail.com>
2025-12-04 16:31:03 -0800

    Reduce size of field tests and omit size checks of  same types

--- generate/ffi/src/fields.rs
+++ generate/ffi/src/fields.rs
@@ -1,10 +1,10 @@
 use crate::Symbols;
-use quote::format_ident;
-use syn::{Block, Fields, Ident, parse_quote};
+use quote::{format_ident, quote};
+use syn::{Block, Fields, Ident, Stmt, parse_quote};
 
 pub fn test_layout(symbols: &Symbols, ident: &Ident, fields: &Fields) -> syn::ItemFn {
     let fn_ident = format_ident!("sys_layout_{}", ident);
-    let mut asserts: Vec<Block> = Vec::with_capacity(fields.len() * 2);
+    let mut asserts: Vec<Stmt> = Vec::with_capacity(fields.len() * 2);
 
     match fields {
         Fields::Named(fields_named) => {
@@ -13,18 +13,28 @@ pub fn test_layout(symbols: &Symbols, ident: &Ident, fields: &Fields) -> syn::It
                 let ty = &f.ty;
                 let sys_ty = crate::prefix_with_sys(symbols, ty);
 
-                asserts.push(parse_quote! {
-                    {
+                let assert_size = if *ty == sys_ty {
+                    None
+                } else {
+                    Some(quote! {
                         assert_eq!(
                             size_of::<#sys_ty>(),
                             size_of::<#ty>()
                         );
+                    })
+                };
+
+                let block: Block = parse_quote! {
+                    {
+                        #assert_size
                         assert_eq!(
                             offset_of!(sys::#ident, #field),
-                            offset_of!(ident, #field),
+                            offset_of!(#ident, #field),
                         );
                     }
-                })
+                };
+
+                asserts.extend(block.stmts);
             }
         }
         Fields::Unnamed(_fields_unnamed) => {
