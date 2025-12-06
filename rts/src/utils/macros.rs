9ab6e5eb9f5c8300c2b4bf43145a00bca3566df2
cydparser <cydparser@gmail.com>
2025-12-05 16:27:06 -0800

    Permit transmutation in `sys!`

--- rts/src/utils/macros.rs
+++ rts/src/utils/macros.rs
@@ -1,12 +1,24 @@
-/// Calls _sys::_ function when _sys_ feature is enabled. Emits `unimplemented` otherwise.
+/// Calls _sys::_ function when _sys_ feature is enabled. Emits `unimplemented` otherwise. This is
+/// only intended to be used by generated code.
 macro_rules! sys {
-    ($fn:ident $($rest:tt)*) => {
+    (@ $fn:ident $($expr:tt)*) => {
         #[cfg(feature = "sys")]
         unsafe {
-            sys::$fn$($rest)*
+            #[allow(clippy::missing_transmute_annotations)]
+            $($expr)*
         }
         #[cfg(not(feature = "sys"))]
         unimplemented!(stringify!($fn))
+    };
+    (transmute($fn:ident $($rest:tt)*)) => {
+        sys! {
+            @ $fn transmute(sys::$fn $($rest)*)
+        }
+    };
+    ($fn:ident $($rest:tt)*) => {
+        sys! {
+            @ $fn sys::$fn $($rest)*
+        }
     };
 }
 
