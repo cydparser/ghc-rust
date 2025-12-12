2330902e00813bc44c9fc7be09b158a91b4bf681
cydparser <cydparser@gmail.com>
2025-12-11 20:36:01 -0800

    Add generate-headers main

---
+++ generate/headers/src/main.rs
@@ -0,0 +1,22 @@
+use std::{env, path::Path};
+
+use cbindgen::Language;
+
+fn main() {
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    let project_dir = manifest_dir.parent().unwrap().parent().unwrap();
+
+    cbindgen::Builder::new()
+        .with_pragma_once(true)
+        .with_language(Language::C)
+        .with_include("target.h")
+        .with_cpp_compat(true)
+        .with_define("target_pointer_width", "64", "SIZEOF_VOID_P_8")
+        .with_define("target_pointer_width", "32", "SIZEOF_VOID_P_4")
+        .with_crate(project_dir.join("rts"))
+        .with_parse_expand_default_features(false)
+        .generate()
+        .expect("Unable to generate header")
+        .write_to_file("include/Rts.h");
+}
