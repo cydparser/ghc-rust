70fd6ca9f410520efdb265c9553560f5265be48b
cydparser <cydparser@gmail.com>
2025-12-24 17:05:29 -0800

    Add TODO for `barf`

--- rts/src/ffi/rts/messages.rs
+++ rts/src/ffi/rts/messages.rs
@@ -1,5 +1,9 @@
 use crate::prelude::*;
 
+// TODO(rust): `barf` is exposed in one place. Stable Rust does not variadic functions.
+//   libraries/ghc-internal/cbits/inputReady.c:
+//      barf("fdReady: fd is too big: %d but FD_SETSIZE is %d", fd, (int)FD_SETSIZE);
+
 #[ffi(compiler)]
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn rtsOutOfBoundsAccess() -> ! {
