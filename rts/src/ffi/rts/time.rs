2c4574f7c2b2d417cc4f79cb0bff2afbe2bd3088
cydparser <cydparser@gmail.com>
2026-05-03 13:38:40 -0700

    Add USToTime and fsecondsToTime

--- rts/src/ffi/rts/time.rs
+++ rts/src/ffi/rts/time.rs
@@ -5,9 +5,24 @@ pub use crate::win32::get_time::getProcessElapsedTime;
 #[cfg(test)]
 mod tests;
 
-pub(crate) const TIME_RESOLUTION: u32 = 1000000000;
+pub(crate) const TIME_RESOLUTION: Time = 1000000000;
 
 pub(crate) const TIME_MAX: u64 = 9223372036854775807;
 
 #[ffi(compiler, ghc_lib, libraries)]
 pub type Time = i64;
+
+#[doc(alias = "USToTime")]
+macro_rules! us_to_time {
+    ($expr:expr) => {
+        (t as Time) * 1000 as Time
+    };
+}
+
+pub(crate) use us_to_time;
+
+/// Use instead of SecondsToTime() when we have a floating-point
+/// seconds value, to avoid truncating it.
+pub(crate) fn fsecondsToTime(t: f64) -> Time {
+    (t * TIME_RESOLUTION as f64) as Time
+}
