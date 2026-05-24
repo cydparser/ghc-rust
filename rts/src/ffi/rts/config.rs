447c0a9dcea8634deb9e2fd7a3b4a5f49120e6d7
cydparser <cydparser@gmail.com>
2026-05-24 16:00:39 -0700

    Adjust constants

--- rts/src/ffi/rts/config.rs
+++ rts/src/ffi/rts/config.rs
@@ -1,5 +1,10 @@
 pub(crate) const RTS_USER_SIGNALS: u32 = 1;
 
-pub(crate) const MAX_N_CAPABILITIES: u32 = 256;
+pub(crate) const MAX_N_CAPABILITIES: usize = 256;
 
-pub(crate) const CACHELINE_SIZE: usize = 128;
+pub(crate) const CACHELINE_SIZE: usize = cfg_select! {
+    target_arch = "s390x" => 256,
+    target_arch = "aarch64" => 128,
+    target_arch = "x86_64" => 64,
+    _ => 32,
+};
