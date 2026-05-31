f3249b1f3869434e01e16602c0c3e0a45f83e378
cydparser <cydparser@gmail.com>
2026-05-31 15:30:23 -0700

    Add time types/functions

--- rts/src/time.rs
+++ rts/src/time.rs
@@ -19,6 +19,12 @@ pub(crate) const TIME_RESOLUTION: Time = 1_000_000_000;
 #[ffi(compiler, ghc_lib, libraries)]
 pub type Time = i64;
 
+/// TODO(rust): Use newtype wrappers for time types.
+pub(crate) type s = i64;
+pub(crate) type ms = i64;
+pub(crate) type us = i64;
+pub(crate) type ns = i64;
+
 #[doc(alias = "USToTime")]
 macro_rules! us_to_time {
     ($expr:expr) => {
@@ -27,6 +33,51 @@ macro_rules! us_to_time {
 }
 
 pub(crate) use us_to_time;
+
+#[inline]
+pub(crate) const fn TimeToMS(t: Time) -> ms {
+    t / 1_000_000
+}
+
+#[inline]
+pub(crate) const fn TimeToUS(t: Time) -> us {
+    t / 1_000
+}
+
+#[inline]
+pub(crate) const fn TimeToNS(t: Time) -> ns {
+    t
+}
+
+#[inline]
+pub(crate) const fn MSToTime(ms: ms) -> Time {
+    ms * 1_000_000
+}
+
+#[inline]
+pub(crate) const fn USToTime(us: us) -> Time {
+    us * 1_000
+}
+
+#[inline]
+pub(crate) const fn NSToTime(ns: ns) -> Time {
+    ns
+}
+
+#[inline]
+pub(crate) const fn SecondsToTime(s: s) -> Time {
+    s * TIME_RESOLUTION
+}
+
+#[inline]
+pub(crate) const fn TimeToSeconds(time: Time) -> s {
+    time / TIME_RESOLUTION
+}
+
+#[inline]
+pub(crate) const fn TimeToSecondsDbl(time: Time) -> f64 {
+    time as f64 / TIME_RESOLUTION as f64
+}
 
 /// Use instead of SecondsToTime() when we have a floating-point
 /// seconds value, to avoid truncating it.
